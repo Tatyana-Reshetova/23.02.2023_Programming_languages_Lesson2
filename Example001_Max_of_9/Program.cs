af3263a5e9f8110087e52d432a522101a04799ec
Tatyana_Reshetova <vlasenko2205@gmail.com>
2023-02-23 15:15:23 +0300

    Решение 2 с помощью функции

--- Example001_Max_of_9/Program.cs
+++ Example001_Max_of_9/Program.cs
@@ -1,4 +1,13 @@
-﻿// Максимальное число из 9 чисел
+﻿// Максимальное число из 9 чисел (решение с помощью функции)
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2>result) result=arg2;
+    if (arg3>result) result=arg3;
+    return result;
+}
+
 int a1 = 15;
 int b1 = 21;
 int c1 = 39;
@@ -9,14 +18,13 @@ int a3 = 13;
 int b3 = 12347;
 int c3 = 33;
 
-int max = a1;
-if (b1>max) max = b1;
-if (c1>max) max = c1;
-if (a2>max) max = a2;
-if (b2>max) max = b2;
-if (c2>max) max = c2;
-if (a3>max) max = a3;
-if (b3>max) max = b3;
-if (c3>max) max = c3;
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
 
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
 Console.WriteLine(max);
